1e6aeaf1548f3636829d47ca24913025eb308cef
b1uesoda <b1uesoda@naver.com>
2024-03-29 10:07:50 +0900

    PostsService

--- src/main/java/com/b1uesoda/springboot/service/posts/PostsService.java
+++ src/main/java/com/b1uesoda/springboot/service/posts/PostsService.java
@@ -1,4 +1,14 @@
 package com.b1uesoda.springboot.service.posts;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
