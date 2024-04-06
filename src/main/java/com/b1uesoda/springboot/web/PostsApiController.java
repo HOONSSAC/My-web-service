ad9abf83c5da32c867411a5fa24568e211b25cb2
b1uesoda <b1uesoda@naver.com>
2024-04-06 12:14:43 +0900

    add update and find mapping

--- src/main/java/com/b1uesoda/springboot/web/PostsApiController.java
+++ src/main/java/com/b1uesoda/springboot/web/PostsApiController.java
@@ -3,9 +3,7 @@ package com.b1uesoda.springboot.web;
 import com.b1uesoda.springboot.service.posts.PostsService;
 import com.b1uesoda.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // 게시글 등록을 위한 PostsApiController 클래스
 @RequiredArgsConstructor
@@ -17,5 +15,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
