305da0f67c80446c9afb625bc0aeabe41db54c16
b1uesoda <b1uesoda@naver.com>
2024-04-08 16:02:47 +0900

    Posts Response Dto

--- src/main/java/com/b1uesoda/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/b1uesoda/springboot/web/dto/PostsResponseDto.java
@@ -1,4 +1,21 @@
 package com.b1uesoda.springboot.web.dto;
 
+import com.b1uesoda.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+// 게시글 조회 시 필요한 응답 DTO
+@Getter
 public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    // PostsResponseDto는 Entity의 필드 중 일부만 사용하므로 생성자로 Entity를 받아 필드에 값을 넣음
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
 }
