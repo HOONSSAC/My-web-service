5e4eaa1c63f61a04f377e5437b10e4d4d7e351d9
b1uesoda <b1uesoda@naver.com>
2024-04-08 16:09:54 +0900

    Posts Update Request Dtog

---
+++ src/main/java/com/b1uesoda/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.b1uesoda.springboot.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
