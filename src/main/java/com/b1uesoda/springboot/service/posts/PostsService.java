c0efa54f042e4b28734294b77ac958977436eb21
b1uesoda <b1uesoda@naver.com>
2024-04-08 16:30:26 +0900

    add update and findById method

--- src/main/java/com/b1uesoda/springboot/service/posts/PostsService.java
+++ src/main/java/com/b1uesoda/springboot/service/posts/PostsService.java
@@ -1,7 +1,11 @@
 package com.b1uesoda.springboot.service.posts;
 
+import com.b1uesoda.springboot.domain.posts.Posts;
 import com.b1uesoda.springboot.domain.posts.PostsRepository;
+import com.b1uesoda.springboot.web.dto.PostsResponseDto;
 import com.b1uesoda.springboot.web.dto.PostsSaveRequestDto;
+import com.b1uesoda.springboot.web.dto.PostsUpdateRequestDto;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +17,21 @@ public class PostsService {
     // 게시글 저장
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
