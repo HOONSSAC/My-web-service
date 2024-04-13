49fc78d0a97534b2dec6a27960fdba1ed8a04024
b1uesoda <b1uesoda@naver.com>
2024-04-13 17:13:13 +0900

    add JPA Auditing

--- src/main/java/com/b1uesoda/springboot/Application.java
+++ src/main/java/com/b1uesoda/springboot/Application.java
@@ -1,7 +1,9 @@
 package com.b1uesoda.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/b1uesoda/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/b1uesoda/springboot/domain/BaseTimeEntity.java
@@ -1,4 +1,23 @@
 package com.b1uesoda.springboot.domain;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
 }
