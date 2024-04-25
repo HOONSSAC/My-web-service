c6e6f898e949a5c02a6f5a89ee03c488c7d326c6
b1uesoda <b1uesoda@naver.com>
2024-04-26 00:04:08 +0900

    add post save method

--- src/main/java/com/b1uesoda/springboot/web/IndexController.java
+++ src/main/java/com/b1uesoda/springboot/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.b1uesoda.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
